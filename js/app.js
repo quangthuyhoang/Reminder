ef17200eb2e3bb7174157d1a918955a4af4fd663
quangthuyhoang <qtnhoang@gmail.com>
2019-03-29 22:03:14 -0500

    added active row selection and cleared input on scan upc

--- js/app.js
+++ js/app.js
@@ -2,14 +2,15 @@
 const environment = 'production';
 // const environment = 'development';
 
-const input = document.querySelector("#mainInput");
+const input = document.querySelector("#messageInput");
+const upcInput = document.querySelector("#mainInput");
 const enterBtn = document.querySelector("#enterBtn");
 const rList = document.querySelector("#reminderList");
 const loading = document.querySelector('#spinner');
 const datatable = document.querySelector('#maintable');
 const headers = document.querySelector('#datatable');
 const reminders = rList;
-console.log("loading", loading)
+const activeSelection = document.querySelector('#activeSelection');
 //********************   CONTROLLERS ***********************
 const displayInput = () => {
   console.log(input.value)
@@ -17,28 +18,34 @@ const displayInput = () => {
   // console.log(reminders.inner)
 }
 
-const addRowToTable = (args) => {
 
-  console.log(`adding to ${datatable.rows.length + 1} row for datatable`)
+const addRowToTable = (args) => {
 
   const fields = Object.values(args);
   const rowIndex = datatable.rows.length;
+  
   let row = datatable.insertRow(rowIndex);
-
+  row.id = "item-" + rowIndex;
+  row.className = "items";
+  
   for(let j = 0; j < datatable.rows[0].cells.length; j++) {
+
     let cell = row.insertCell(j);
     if (j === 0) {
       cell.className = "mdl-data-table__cell--non-numeric";
     }
     cell.innerText = fields[j];
   }
+  row.addEventListener('click', function() {
+
+    const rowNode = document.querySelector('#' + row.id);
+    activeSelection.innerHTML = `<div>${fields}</div>`;
+  })
 }
 
-addRowToTable({
-  Name: 'Food',
-  Quantity: 3,
-  Date: 'YYYY-MM-DD'
-})
+const clearInput = (inputComponent) => {
+  inputComponent.value = "";
+}
 
 const addReminder = (args) => {
 
@@ -53,6 +60,29 @@ const url = () => {
     return 'https://dramatic-tartan.glitch.me/';
   }
   return 'http://localhost:3000/'
+}
+
+const inventoryURL = () => {
+  return 'https://grandiose-pine.glitch.me/';
+}
+
+const captureUPC = (event) => {
+  if (isUPC(event)) {
+    // TODO: generate obj to send to api
+    createInventory(event)
+    clearInput(upcInput);
+  }
+}
+
+const isUPC = (arg) => {
+  const args = arg;
+  var isUPC = false;
+  if (args) {
+    if (typeof args === 'string' && args.length === 8 && /^\d+$/.test(args)) {
+      isUPC = true;
+    }
+  }
+  return isUPC;
 }
 
 const createToDoList = (todos) => {
@@ -79,8 +109,24 @@ const hideLoader = () => {
   loading.innerHTML = ``;
 }
 
+
+
+const addEventListenerTo = (classSelector, action, fn) => {
+  const selectors = document.querySelectorAll(classSelector);
+  console.log('selector', selectors)
+  const num = selectors.length;
+  if (num > 1) {
+    selectors.forEach(function(selector) {
+      selector.addEventListener(action, fn(event, selector.id))
+    })
+  }
+}
+
+  // ================= ASYNC API Request ===============
+
+  // MESSAGE
 const getTodoList = (name) => {
-  console.log(`waiting on data`)
+
   displayLoader();
   return fetch(url() + 'username/' + name, {
         method: "GET", // *GET, POST, PUT, DELETE, etc.
@@ -93,7 +139,6 @@ const getTodoList = (name) => {
         // }
       })
   .then(results => {
-  console.log(results)
     return results.json()
   })
   .then(data => {
@@ -111,7 +156,6 @@ const createMessage = (args) => {
     messageorder: args.messageorder,
     message: args.message
   }
-
   fetch(url() + 'message/create', {
     method: 'POST',
     body: JSON.stringify(message),
@@ -129,6 +173,66 @@ const createMessage = (args) => {
     addReminder(formatMessage(data))
   })
 }
+
+// INVENTORY
+const getInventory = (name) => {
+
+  displayLoader();
+  return fetch(inventoryURL() + 'username/' + name, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+      })
+  .then(results => {
+    return results.json()
+  })
+  .then(data => {
+    console.log("finihs", data)
+    for(let k = 0; k < data.length; k++) {
+      addRowToTable({
+        upc: data[k].upc,
+        quantity: data[k].quantity,
+        date: data[k].date
+      })
+    }
+    // // console.log('d', document.querySelectorAll('.items'))
+    // addEventListenerTo('.items', 'click', function(event, id) {
+    //   console.log("click", id)
+    // })
+    // addRowToTable(data)
+    // hideLoader();
+    // createToDoList(data);
+  })
+  .catch(err => console.error(err))
+}
+
+getInventory('Quang')
+
+const createInventory = (upc) => {
+  const inventory = {
+    upc: upc,
+    quantity: 1,
+    date: new Date().toLocaleDateString()
+  }
+
+  
+  fetch(inventoryURL() + 'item/create', {
+    method: 'POST',
+    body: JSON.stringify(inventory),
+		headers: {
+      'Content-Type': 'application/json'
+		}
+  })
+  .then(results => {
+    console.log("results", results)
+    return results.json()
+  })
+  .then(data => {
+    console.log("add to table inventory row", data)
+    addRowToTable(inventory)
+  })
+  .catch(err => {
+    console.log("Error:", err);
+  })
+}
 //******************** EVENT LISTENERS ***********************
 
 enterBtn.addEventListener('click',function() {
@@ -141,13 +245,16 @@ enterBtn.addEventListener('click',function() {
   createMessage(payload);
 });
 
+
+
+// items.forEach(function(item) {
+//   console.log("item", item)
+//   item.addEventListener('click', function() {
+//     console.log("click")
+//   })
+// })
+
+
+
 getTodoList('Quang');
-
-
-
-
-
-
-
-
 
